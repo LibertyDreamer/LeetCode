30f39b6b147c7278d0b4163823689310cdbb674d
LibertyDreamer <138883263+LibertyDreamer@users.noreply.github.com>
2024-01-18 16:50:48 +0100

    Create Question2423.cpp

---
+++ Question2423.cpp
@@ -0,0 +1,50 @@
+/*
+You are given a 0-indexed string word, consisting of lowercase English letters. You need to select one index and remove the letter at that index from word so that the frequency of every letter present in word is equal.
+Return true if it is possible to remove one letter so that the frequency of all letters in word are equal, and false otherwise.
+
+Note:
+The frequency of a letter x is the number of times it occurs in the string.
+You must remove exactly one letter and cannot choose to do nothing.
+ 
+
+Example 1:
+Input: word = "abcc"
+Output: true
+Explanation: Select index 3 and delete it: word becomes "abc" and each character has a frequency of 1.
+
+Example 2:
+Input: word = "aazz"
+Output: false
+Explanation: We must delete a character, so either the frequency of "a" is 1 and the frequency of "z" is 2, or vice versa. It is impossible to make all present letters have equal frequency.
+ 
+Constraints:
+2 <= word.length <= 100
+word consists of lowercase English letters only.
+*/
+
+class Solution {
+    char frequency[26] ;
+public:
+    bool equalFrequency(string word) {
+        for(int i = 0; i < word.size(); ++i)
+        {
+            frequency[word[i]-97]++;
+        }
+
+        for(int j = 0; j < 26; ++j)
+        {
+            if(frequency[j] <= 0) continue;
+            frequency[j]--;
+            std::set<char> s;
+            for(int i = 0; i < 26; ++i)
+            {
+                if(frequency[i] <= 0) continue;
+                s.insert(frequency[i]);
+            }
+            if(s.size() == 1) return true;
+            frequency[j]++;
+        }
+
+        return false;
+    }
+};
