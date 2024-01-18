9ae353b74bc7ad1dfb14318ec6e42196d59195cb
LibertyDreamer <138883263+LibertyDreamer@users.noreply.github.com>
2024-01-18 22:29:59 +0100

    Create Question1114.cpp

---
+++ Question1114.cpp
@@ -0,0 +1,56 @@
+/*
+Suppose we have a class:
+
+public class Foo {
+  public void first() { print("first"); }
+  public void second() { print("second"); }
+  public void third() { print("third"); }
+}
+The same instance of Foo will be passed to three different threads. Thread A will call first(), thread B will call second(), and thread C will call third(). Design a mechanism and modify the program to ensure that second() is executed after first(), and third() is executed after second().
+
+Note:
+We do not know how the threads will be scheduled in the operating system, even though the numbers in the input seem to imply the ordering. The input format you see is mainly to ensure our tests' comprehensiveness.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: "firstsecondthird"
+Explanation: There are three threads being fired asynchronously. The input [1,2,3] means thread A calls first(), thread B calls second(), and thread C calls third(). "firstsecondthird" is the correct output.
+
+Example 2:
+Input: nums = [1,3,2]
+Output: "firstsecondthird"
+Explanation: The input [1,3,2] means thread A calls first(), thread B calls third(), and thread C calls second(). "firstsecondthird" is the correct output.
+ 
+
+Constraints:
+nums is a permutation of [1, 2, 3].
+*/
+
+class Foo {
+    mutex p_2, p_3;
+public:
+    Foo() {
+        p_2.lock();
+        p_3.lock();
+    }
+
+    void first(function<void()> printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        p_2.unlock();
+    }
+
+    void second(function<void()> printSecond) {
+        p_2.lock();
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        p_3.unlock();
+    }
+
+    void third(function<void()> printThird) {
+        p_3.lock();
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};
