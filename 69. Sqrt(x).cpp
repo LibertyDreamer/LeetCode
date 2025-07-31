cb91cd3e4e1d945c93b16f170d8f836a0fa0a668
LibertyDreamer <138883263+LibertyDreamer@users.noreply.github.com>
2025-07-31 19:06:26 +0200

    Create 69. Sqrt(x).cpp

---
+++ 69. Sqrt(x).cpp
@@ -0,0 +1,51 @@
+/*
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+    For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+ 
+
+Constraints:
+
+    0 <= x <= 2^31 - 1
+
+*/
+class Solution {
+public:
+    int mySqrt(int x) {
+        int size = 23170 / 2;
+        int i = 23170;
+
+        while (size > 0) {
+            int square = i * i;
+            if (square == x) {
+                return i;
+            } else if (square > x) {
+                i -= size;
+            } else {
+                i += size;
+            }
+            size /= 2;
+        }
+
+        while (i * i > x) i--;
+        while (((long long)i + 1) * ((long long)i + 1) <= x) i++;
+        return i;
+    }
+};
