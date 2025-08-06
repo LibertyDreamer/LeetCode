0e7569065ab075e2239b0c35a402bd2a96b46b71
LibertyDreamer <138883263+LibertyDreamer@users.noreply.github.com>
2025-08-06 10:07:55 +0200

    Create 1232. Check If It Is a Straight Line.cpp

---
+++ 1232. Check If It Is a Straight Line.cpp
@@ -0,0 +1,45 @@
+/*
+You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+
+ 
+
+ 
+
+Example 1:
+
+Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+Output: true
+
+Example 2:
+
+Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+Output: false
+
+ 
+
+Constraints:
+
+    2 <= coordinates.length <= 1000
+    coordinates[i].length == 2
+    -10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+    coordinates contains no duplicate point.
+*/
+
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        if(coordinates.size() == 2) return true;
+
+        for(int i = 3; i <= coordinates.size(); ++i)
+        {   int& x1 = coordinates[i-3][0];
+            int& x2 = coordinates[i-2][0];
+            int& x3 = coordinates[i-1][0];
+            int& y1 = coordinates[i-3][1];
+            int& y2 = coordinates[i-2][1];
+            int& y3 = coordinates[i-1][1];
+            if((x2-x1)*(y3-y1) != (y2-y1)*(x3-x1)) return false;
+        }
+
+        return true;
+    }
+};
