93ec9e64c17d58cdf84d36ce8cb1ccfbd0aaa4d5
LibertyDreamer <138883263+LibertyDreamer@users.noreply.github.com>
2025-08-21 12:59:11 +0200

    Create 101. Symmetric Tree.cpp

---
+++ 101. Symmetric Tree.cpp
@@ -0,0 +1,72 @@
+/*
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+ 
+
+Example 1:
+
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [1, 1000].
+    -100 <= Node.val <= 100
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        std::queue<TreeNode*> left;
+        std::queue<TreeNode*> right;
+        
+        left.push(root->left);
+        right.push(root->right);
+        
+        while (!left.empty() or !right.empty()) {
+            TreeNode* l = left.front();
+            left.pop();
+            TreeNode* r = right.front();
+            right.pop();
+
+            if (l == nullptr and r == nullptr)
+                continue;
+
+            if (l == nullptr or r == nullptr)
+                return false;
+
+            if (l->val != r->val)
+                return false;
+
+            left.push(l->left);
+            left.push(l->right);
+
+            right.push(r->right);
+            right.push(r->left);
+        }
+
+        if (left.size() != right.size())
+            return false;
+        
+        return true;
+    }
+};
