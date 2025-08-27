87c7bf24addf0dc6c4410aa2e356e640886842b2
LibertyDreamer <138883263+LibertyDreamer@users.noreply.github.com>
2025-08-27 12:05:19 +0200

    Update 154. Find Minimum in Rotated Sorted Array II.cpp

--- 154. Find Minimum in Rotated Sorted Array II.cpp
+++ 154. Find Minimum in Rotated Sorted Array II.cpp
@@ -1,34 +1,26 @@
 /*
-Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,4,4,5,6,7] might become:
 
-    [4,5,6,7,0,1,2] if it was rotated 4 times.
-    [0,1,2,4,5,6,7] if it was rotated 7 times.
+    [4,5,6,7,0,1,4] if it was rotated 4 times.
+    [0,1,4,4,5,6,7] if it was rotated 7 times.
 
 Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
 
-Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+Given the sorted rotated array nums that may contain duplicates, return the minimum element of this array.
 
-You must write an algorithm that runs in O(log n) time.
+You must decrease the overall operation steps as much as possible.
 
  
 
 Example 1:
 
-Input: nums = [3,4,5,1,2]
+Input: nums = [1,3,5]
 Output: 1
-Explanation: The original array was [1,2,3,4,5] rotated 3 times.
 
 Example 2:
 
-Input: nums = [4,5,6,7,0,1,2]
+Input: nums = [2,2,2,0,1]
 Output: 0
-Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
-
-Example 3:
-
-Input: nums = [11,13,15,17]
-Output: 11
-Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
 
  
 
@@ -37,8 +29,11 @@ Constraints:
     n == nums.length
     1 <= n <= 5000
     -5000 <= nums[i] <= 5000
-    All the integers of nums are unique.
     nums is sorted and rotated between 1 and n times.
+
+ 
+
+Follow up: This problem is similar to Find Minimum in Rotated Sorted Array, but nums may contain duplicates. Would this affect the runtime complexity? How and why?
 
 */
 
@@ -46,6 +41,19 @@ Constraints:
 class Solution {
 public:
     int findMin(vector<int>& nums) {
+        if(nums[0] == nums[nums.size()-1])
+        {
+            int min = nums[0];
+
+            for(int i = 0; i < nums.size(); ++i)
+            {
+                min = std::min(nums[i],min);
+            }
+
+            return min;
+        }
+
+
         int left = 0;
         int right = nums.size()-1;
         int middle;
