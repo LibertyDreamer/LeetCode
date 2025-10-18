b8b412c05e6a129d1bba22e7d748606b99540f04
LibertyDreamer <138883263+LibertyDreamer@users.noreply.github.com>
2025-10-18 10:31:40 +0200

    Add function to calculate minimum absolute difference in BST
    Implement a function to find the minimum absolute difference between values of any two nodes in a BST.

---
+++ (TODO: resolve)530. Minimum Absolute Difference in BST.cpp
@@ -0,0 +1,73 @@
+/*
+Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+
+ 
+
+Example 1:
+
+Input: root = [4,2,6,1,3]
+Output: 1
+
+Example 2:
+
+Input: root = [1,0,48,null,null,12,49]
+Output: 1
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [2, 104].
+    0 <= Node.val <= 105
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int getMinimumDifference(TreeNode* root) {
+        std::queue<TreeNode*> q;
+        q.push(root);
+
+        std::set<int> s;
+
+        while (!q.empty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = q.front();
+                s.insert(node->val);
+                if (node->left) {
+
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+
+                q.pop();
+            }
+        }
+
+        int prev = *s.begin();
+        auto itr = s.begin();
+        itr++;
+        int min = INT_MAX;
+        for (; itr != s.end(); itr++) {
+            min = std::min(min, *itr - prev);
+            prev = *itr;
+        }
+
+        return min;
+    }
+};
