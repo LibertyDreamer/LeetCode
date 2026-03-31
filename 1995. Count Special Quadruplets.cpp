2528c0da5009792430c4fd3dc557a7a4faf2e23f
LibertyDreamer <138883263+LibertyDreamer@users.noreply.github.com>
2026-03-31 22:01:17 +0200

    Create 1995. Count Special Quadruplets.cpp

---
+++ 1995. Count Special Quadruplets.cpp
@@ -0,0 +1,57 @@
+/*
+Given a 0-indexed integer array nums, return the number of distinct quadruplets (a, b, c, d) such that:
+
+    nums[a] + nums[b] + nums[c] == nums[d], and
+    a < b < c < d
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,6]
+Output: 1
+Explanation: The only quadruplet that satisfies the requirement is (0, 1, 2, 3) because 1 + 2 + 3 == 6.
+
+Example 2:
+
+Input: nums = [3,3,6,4,5]
+Output: 0
+Explanation: There are no such quadruplets in [3,3,6,4,5].
+
+Example 3:
+
+Input: nums = [1,1,1,3,5]
+Output: 4
+Explanation: The 4 quadruplets that satisfy the requirement are:
+- (0, 1, 2, 3): 1 + 1 + 1 == 3
+- (0, 1, 3, 4): 1 + 1 + 3 == 5
+- (0, 2, 3, 4): 1 + 1 + 3 == 5
+- (1, 2, 3, 4): 1 + 1 + 3 == 5
+
+ 
+
+Constraints:
+
+    4 <= nums.length <= 50
+    1 <= nums[i] <= 100
+*/
+
+class Solution {
+public:
+    int countQuadruplets(vector<int>& nums) {
+        int quadruplets = 0;
+        int b_c;
+        for (size_t d = nums.size() - 1; d >= 3; --d)
+            for (size_t c = d - 1; c >= 2; --c)
+                for (size_t b = c - 1; b >= 1; --b) {
+                    b_c = nums[b] + nums[c];
+                    for (int a = static_cast<int>(b - 1); a >= 0; --a) {
+                        if (nums[a] + b_c == nums[d])
+                            quadruplets++;
+                    }
+                }
+
+        return quadruplets;
+    }
+};
+
